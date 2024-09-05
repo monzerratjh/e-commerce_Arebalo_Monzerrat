325d9656d56ad35d29a43506bb217620750f7889
Monze <monzerratarebalojh@gmail.com>
2024-09-05 20:35:58 -0300

    Agregaciones del primer dinamismo
    Agregación del título h1 por medio del document.querySelector("");
    Creación del arreglo cardsArray para poner las cartas de la Home Page.
    Creación dle bucle for para creación de las cartas con imágenes de forma dinámica. ¿Qué falta? Faltaría agregar el comando para poder agregarlo al DOM.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Holassss"); /*Console.log es un método*/ 
+const h1 = document.querySelector(".h1Main");
+h1.innerText = "Productos";
+
+let cardsArray = [];
+for (let i = 1; i < 10; i++) {
+    cardsArray.push(` 
+        <div class="card" style="width: 18rem;">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Tarjeta numero ${i}.</p>
+            </div>
+        </div>
+    `); //batiks para el contenido en diferentes líneas, ${i} para mostrar la posición/valor en el arreglo. 
+    let container = document.querySelector(".container"); //seleccionamos para poder agregar la card.
+    
+    /*container = document.querySelector(“.container”).append = “<div>...</div>”;?????????????????????*/
+
+    console.log("Intento " + i + "para ver si funciona");
+}

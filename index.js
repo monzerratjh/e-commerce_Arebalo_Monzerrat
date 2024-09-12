704a83c926d81202dcd0191cf67a718ff36c059a
Monze <monzerratarebalojh@gmail.com>
2024-09-12 18:30:50 -0300

    SPRINT

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const h1 = document.querySelector(".h1Main");
-h1.innerText = "Productos";
+const h1 = document.querySelector("h1");
+h1.innerText = "Productos"; //le da valor al h1
 
-let cardsArray = [];
+let cardsArray = []; //creamos un arreglo en dónde poner las cards
 for (let i = 1; i < 10; i++) {
-    cardsArray.push(` 
+    cardsArray.push(`
         <div class="card" style="width: 18rem;">
             <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
             <div class="card-body">
@@ -11,9 +11,9 @@ for (let i = 1; i < 10; i++) {
             </div>
         </div>
     `); //batiks para el contenido en diferentes líneas, ${i} para mostrar la posición/valor en el arreglo. 
-    let container = document.querySelector(".container"); //seleccionamos para poder agregar la card.
-    
-    /*container = document.querySelector(“.container”).append = “<div>...</div>”;?????????????????????*/
-
-    console.log("Intento " + i + "para ver si funciona");
+     //seleccionamos para poder agregar la card en el container.
 }
+document.querySelector(".container").innerHTML = cardsArray.join(''); 
+/*Si pusieramos "document.querySelector(".container").innerHTML = cardsArray.join('');" dentro del bucle, estaríamos modificando el 
+contenido del contenedor en cada vuelta del ciclo. Por cada tarjeta nueva, estaríamos reemplazando todo el contenido del contenedor.*/
+

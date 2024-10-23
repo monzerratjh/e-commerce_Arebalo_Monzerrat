2d179d6409c035e3fda8f490b2cd2732c3056aab
Monze <monzerratarebalojh@gmail.com>
2024-10-23 16:02:27 -0300

    Botones de categoría (SPRINT 2)

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const data = [
       detalle: "Descripción del producto 1.",
       precio: 199.99,
       stock: 10,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg",
+      category: "ferrari"
   },
   {
       id: 2,
@@ -14,7 +15,8 @@ const data = [
       detalle: "Descripción del producto 2.",
       precio: 299.99,
       stock: 5,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/2.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/2.jpg",
+      category: "mclaren"
   },
   {
       id: 3,
@@ -22,7 +24,8 @@ const data = [
       detalle: "Descripción del producto 3.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/3.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/3.jpg",
+      category: "honda"
   },
   {
       id: 4,
@@ -30,7 +33,8 @@ const data = [
       detalle: "Descripción del producto 4.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/4.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/4.jpg",
+      category: "ferrari"
   },
   {
       id: 5,
@@ -38,7 +42,8 @@ const data = [
       detalle: "Descripción del producto 5.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/5.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/5.jpg",
+      category: "mclaren"
   },
   {
       id: 6,
@@ -46,7 +51,8 @@ const data = [
       detalle: "Descripción del producto 6.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/6.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/6.jpg",
+      category: "honda"
   },
   {
       id: 7,
@@ -54,7 +60,8 @@ const data = [
       detalle: "Descripción del producto 7.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/7.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/7.jpg",
+      category: "ferrari"
   },
   {
       id: 8,
@@ -62,7 +69,8 @@ const data = [
       detalle: "Descripción del producto 8.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/8.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/8.jpg",
+      category: "mclaren"
   },
   {
       id: 9,
@@ -70,58 +78,68 @@ const data = [
       detalle: "Descripción del producto 9.",
       precio: 149.99,
       stock: 15,
-      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/9.jpg"
+      imagen: "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/9.jpg",
+      category: "honda"
   }
 ];
 
-//FUNCIÓN PARA MOSTRAR LAS CARDS EN EL MAIN
+// FUNCIÓN PARA MOSTRAR LAS CARDS EN EL MAIN
 function displayProducts(products) {
-  const productList = document.getElementById('product-list');
-  productList.innerHTML = ''; // Limpiar la lista antes de añadir nuevos productos
-  products.forEach(product => {
+    const productList = document.getElementById('product-list');
+    productList.innerHTML = ''; // Limpiar la lista antes de añadir nuevos productos
+    products.forEach(product => {
       productList.innerHTML += `
-          <div class="col-4">
-              <div class="card">
-                  <img src="${product.imagen}" class="card-img-top" alt="${product.titulo}">
-                  <div class="card-body">
-                      <h5 class="card-title">${product.titulo}</h5>
-                      <p class="card-text">${product.detalle}</p>
-                      <p>Precio: $${product.precio.toFixed(2)}</p>
-                      <a href="./producto.html?prod=${product.id}" class="btn btn-primary">Ver más</a>
-                  </div>
-              </div>
-          </div>
+        <div class="col-4">
+            <div class="card">
+                <img src="${product.imagen}" class="card-img-top" alt="${product.titulo}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.titulo}</h5>
+                    <p class="card-text">${product.detalle}</p>
+                    <p>Precio: $${product.precio.toFixed(2)}</p>
+                    <a href="./producto.html?prod=${product.id}" class="btn btn-primary">Ver más</a>
+                </div>
+            </div>
+        </div>
       `;
-  });
-}
-
-//Cargamos los productos y ponemos data como parámetro.
-displayProducts(data);
-
-//Captura el bot+on de búsqueda y el input
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const clearButton = document.getElementById('clear-button');
-
-// Evento del searchButton
-searchButton.addEventListener('click', () => {
-  const searchTerm = searchInput.value.trim().toLowerCase();
-  const filteredData = data.filter(product => product.titulo.toLowerCase() === searchTerm);
-
-  // Comprobamos si hay productos filtrados, sino, muestra todos los productos
-  if (filteredData.length > 0) {
-    displayProducts(filteredData);  
-  } else {
-    alert("No se encontraron productos.");  
-    displayProducts(data);  
+    });
   }
-});
-
-
-// Evento que limpia el buscador
-clearButton.addEventListener('click', () => {
-searchInput.value = '';
-displayProducts(data); // Volver a mostrar todos los productos
-});
-
-
+  
+  // Cargamos los productos inicialmente
+  displayProducts(data);
+  
+  // FUNCIÓN DE FILTRADO POR CATEGORÍA
+  const filterByCategory = (category) => {
+    const filteredData = category === "todos" ? data : data.filter(product => product.category === category);
+    displayProducts(filteredData);
+  };
+  
+  // EVENTOS de botones de CATEGORÍA
+  const categoryButtons = document.querySelectorAll(".category-btn");
+  categoryButtons.forEach(button => {
+    button.addEventListener("click", () => filterByCategory(button.dataset.category));
+  });
+  
+  // CAPTURAR EL BOTÓN DE BÚSQUEDA Y EL INPUT
+  const searchInput = document.getElementById('search-input');
+  const searchButton = document.getElementById('search-button');
+  const clearButton = document.getElementById('clear-button');
+  
+  // Evento del searchButton
+  searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    const filteredData = data.filter(product => product.titulo.toLowerCase() === searchTerm);
+  
+    // Comprobamos si hay productos filtrados, sino, muestra todos los productos
+    if (filteredData.length > 0) {
+      displayProducts(filteredData);
+    } else {
+      alert("No se encontraron productos.");
+      displayProducts(data);
+    }
+  });
+  
+  // Evento que limpia el buscador
+  clearButton.addEventListener('click', () => {
+    searchInput.value = '';
+    displayProducts(data); // Volver a mostrar todos los productos
+  });
